e610dfda15b58708244f5ea95fb57d6a755a5e7e
Salomé Suárez <salomesalomesuarezrodriguez@gmail.com>
2024-06-25 17:30:19 -0300

    atividades

---
+++ atividades.py
@@ -0,0 +1,99 @@
+
+# nomeProprio = input("Digite seu nome:")
+# print("Olá, "+ nomeProprio)
+# apelido = input("Digite seu apelido:")
+# print("Teu apelido é: "+ apelido)
+# endereco = input("Digite seu endereço:")
+# print("Teu endereço é: "+ endereco)
+# cidadeCodigo = input("Digite sua cidade e código postal:")
+# print("Tua cidade e código postal é: "+ cidadeCodigo)
+# print(f"Teus dados são:{nomeProprio}, {apelido}, {endereco} e {cidadeCodigo}")
+                                                                                                
+
+
+
+# nome = input("Digite seu nome: ")
+# print("nome confirmado, "+ nome )
+# ano = int(input("Digite seu ano: "))
+# print("ano confirmado, ", ano)
+# print(f"Em 2024 você terá: {2024 - ano}")
+# print(f"{nome} é uma pessoa legal, nascida no ano de {ano}. Uma vez,{nome} acordou cedo para ir ao trabalho e aconteceu uma coisa inesperada")
+
+
+
+
+# cidade = "São Paulo"
+# print("A cidade é: "+ cidade)
+# cidade = "Rio de Janeiro"
+# print("Cidade atualizada: "+ cidade)
+
+
+
+
+# variavelString = "String"
+# print(f"Variavel 1 : {type(variavelString)}")
+# variavelInteiro = 3
+# print(f"Variavel 2 : {type(variavelInteiro)}")
+# variavelFloat = 3.6
+# print(f"Variavel 3 : {type(variavelFloat)}")
+
+
+
+
+
+# parte1 = "A vida"
+# print("parte um da frase:"+ parte1)
+# parte2 = " vale a pena?"
+# print("parte dois da frase:"+ parte2)
+# print(f"Frase completa: {parte1}{parte2}")
+
+
+
+
+
+
+# altura = float(input("Qual sua altura? "))
+# peso = float(input("Qual seu peso? "))
+# imc = peso / (altura / 100) ** 2
+# print(f"O IMC é: {imc}")
+
+
+
+
+# numero = int(input("Digite um número: "))
+# calculo = numero * 5 
+# print(f"{numero} vezes 5 é", calculo )
+
+
+
+
+
+
+# nome = input("Digite seu nome: ")
+# print("nome confirmado, "+ nome )
+# ano = int(input("Digite seu ano: "))
+# print("ano confirmado, ", ano)
+# print(f"Olá {nome}, você fará {2024 - ano} anos no final de 2024")
+
+
+
+
+# preco = float(input("Digite preço do produto: "))
+# porcentagem = float(input("Digite porcentagem: "))
+# desconto = preco * porcentagem / 100
+# print(f"O valor do desconto é {desconto} e o novo preço será {preco - desconto}")
+
+
+
+
+# preco = float(input("Digite preço do produto: "))
+# porcentagem = float(input("Digite porcentagem: "))
+# desconto = preco * porcentagem / 100
+# print(f"O valor do desconto é {desconto} e o novo preço será {preco + desconto}")
+
+
+
+
+
+valor = float(input("Digite um valor em reais: "))
+print(f"Em dólares o valor é de {round(valor / 5.45,2)}")
