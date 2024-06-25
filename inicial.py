92518cb5b0ea181fdc82c0170ab251aea4bcbce1
Salomé Suárez <salomesalomesuarezrodriguez@gmail.com>
2024-06-25 14:07:22 -0300

    inicial

---
+++ inicial.py
@@ -0,0 +1,55 @@
+print("Olá Mundo")
+#Meu Primeiro Script Python
+print("Bem vindo à programação")
+print("Primeiro estamos praticando o comando Print")
+print("Esse comando Printa 3 linhas de texto")
+
+
+
+print("Salomé")
+print("Rua Juvenal Carvalho")
+print(17)
+
+
+
+
+
+nome = input("Digite seu nome:")
+print("Olá, "+ nome)
+
+nome = input("Digite seu nome:")
+print("Olá, "+ nome)
+
+
+
+#String F
+resultado = 25
+resultado2 = 30
+
+
+print(f"O resultado da conta é:{resultado}, e a conta de dois valores é {resultado2}" )
+
+print("O resultado da conta é:{}, e a conta de dois valores é {}".format(resultado, resultado2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python.py
@@ -0,0 +1,20 @@
+
+
+nome = "Salome"
+idade = 17
+skill1 = "python"
+level1 = "iniciante"
+skill2 = "java"
+level2 = "senior"
+skill3 = "C++"
+level3 = "pleno"
+baixo = 3000
+alto = 4000
+
+
+
+print(f"meu nome é {nome}, tenho {idade} anos")
+print(f"minhas habilidades são {skill1}, {level1}, {skill2}, {level2}, {skill3} e {level3}")
+print(f"Procuro um emprego com um salário de {baixo} a {alto} euros por mês")
+
+
